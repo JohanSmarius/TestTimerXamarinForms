45a0d398ea19aae65f87a09aea3d3c115ba83b6c
johansmarius <jsmarius@hotmail.com>
2021-12-19 23:04:20 +0100

    First version

---
+++ TestTimer/TestTimer/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TestTimer.Services;
+using TestTimer.Views;
+
+namespace TestTimer
+{
+    public partial class App : Application
+    {
+
+        public App ()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart ()
+        {
+        }
+
+        protected override void OnSleep ()
+        {
+        }
+
+        protected override void OnResume ()
+        {
+        }
+    }
+}
+
---
+++ TestTimer/TestTimer/ViewModels/AboutViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace TestTimer.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+
+        public AboutViewModel()
+        {
+            Title = "About";
+            OpenWebCommand = new Command(() => StartTimer());
+        }
+
+        public ICommand OpenWebCommand { get; }
+
+
+        public void StartTimer()
+        {
+            const int waitTime = 60;
+
+            int currentSpan = 0;
+
+
+
+            Device.StartTimer(new TimeSpan(0, 0, 5), () =>
+            {
+                // do something every 10 seconds
+                currentSpan += 5;
+
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    // interact with UI elements
+                    Title = $"Remaining {waitTime - currentSpan} seconds";
+
+                });
+
+                if (currentSpan == waitTime)
+                {
+                    Vibration.Vibrate();
+                    App.Current.MainPage.DisplayAlert("Timer finished", "Timer is afgelopen", "OK");
+                }
+
+                return currentSpan < waitTime; // runs again, or false to stop
+            });
+        }
+    }
+}
