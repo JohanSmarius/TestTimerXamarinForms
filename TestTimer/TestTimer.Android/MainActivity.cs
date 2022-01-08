316ffb2bdefd66996d232aad25c4616737bb5e77
johansmarius <jsmarius@hotmail.com>
2022-01-08 20:55:19 +0100

    Added new code.

--- TestTimer/TestTimer.Android/MainActivity.cs
+++ TestTimer/TestTimer.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Plugin.LocalNotification;
 
 namespace TestTimer.Droid
 {
@@ -14,9 +15,14 @@ namespace TestTimer.Droid
         {
             base.OnCreate(savedInstanceState);
 
+            NotificationCenter.CreateNotificationChannel(builder => 
+                builder.WithChannelId("ChannelForAlerts").WithImportance(NotificationImportance.Max).WithVibration(true).Build());
+
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
--- TestTimer/TestTimer/ViewModels/AboutViewModel.cs
+++ TestTimer/TestTimer/ViewModels/AboutViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Plugin.LocalNotification;
+using Plugin.LocalNotification.AndroidOption;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
@@ -9,25 +11,47 @@ namespace TestTimer.ViewModels
     public class AboutViewModel : BaseViewModel
     {
 
-        public AboutViewModel()
+        private string _timer1Progress = string.Empty; 
+        public string Timer1Progress
         {
-            Title = "About";
-            OpenWebCommand = new Command(() => StartTimer());
+            get => _timer1Progress;
+            set => SetProperty(ref _timer1Progress, value);
         }
 
-        public ICommand OpenWebCommand { get; }
+        private bool _timer1Running = true;
+        public bool Timer1Running
+        {
+            get => _timer1Running;
+            set => SetProperty(ref _timer1Running, value);
+        }
 
 
+        private string _timer2Progress = string.Empty; 
+        public string Timer2Progress
+        {
+            get => _timer2Progress;
+            set => SetProperty(ref _timer2Progress, value);
+        }
+
+        private bool _timer2Running = true;
+        public bool Timer2Running
+        {
+            get => _timer2Running;
+            set => SetProperty(ref _timer2Running, value);
+        }
+
+
+        public string Type { get; set; }
         public void StartTimer()
         {
             const int waitTime = 60;
 
             int currentSpan = 0;
-
-
-
+            
             Device.StartTimer(new TimeSpan(0, 0, 5), () =>
             {
+                Timer1Running = true;
+
                 // do something every 10 seconds
                 currentSpan += 5;
 
@@ -42,10 +66,132 @@ namespace TestTimer.ViewModels
                 {
                     Vibration.Vibrate();
                     App.Current.MainPage.DisplayAlert("Timer finished", "Timer is afgelopen", "OK");
+                    Timer1Running = false;
                 }
 
                 return currentSpan < waitTime; // runs again, or false to stop
             });
         }
+
+        
+        
+        public AboutViewModel()
+        {
+            Title = "About";
+            StartTimer1Command = new Command(() => StartTimer1());
+            StartTimer2Command = new Command(() => StartTimer2());
+            
+        }
+
+        public ICommand StartTimer1Command { get; }
+        
+        public ICommand StartTimer2Command { get; }
+
+
+        public void StartTimer1()
+        {
+            const int waitTime = 60;
+
+            int currentSpan = 0;
+            
+            Device.StartTimer(new TimeSpan(0, 0, 5), () =>
+            {
+                Timer1Running = false;
+
+                // do something every 10 seconds
+                currentSpan += 5;
+
+
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    // interact with UI elements
+                    Timer1Progress = $"Remaining {waitTime - currentSpan} seconds";
+
+                });
+
+                if (currentSpan == waitTime)
+                {
+                    Vibration.Vibrate();
+
+                    var notification = new NotificationRequest
+                    {
+                        Android = new AndroidOptions
+                        {
+                            ChannelId = "ChannelForAlerts",
+                            Ongoing = false,
+                            Priority = NotificationPriority.Max,
+                            VisibilityType = AndroidVisibilityType.Private,
+                        },
+                        BadgeNumber = 1,
+                        CategoryType = NotificationCategoryType.Alarm,
+                        Description = "Step 1 is completed",
+                        Title = "Step completed",
+                        ReturningData = "This data belongs to step 1",
+                        NotificationId = 10,
+                    };
+
+                    NotificationCenter.Current.Show(notification);
+
+//                    App.Current.MainPage.DisplayAlert("Timer finished", "Timer is afgelopen", "OK");
+                    Timer1Running = true;
+                }
+
+                return currentSpan < waitTime; // runs again, or false to stop
+            });
+        }
+        
+
+        public void StartTimer2()
+        {
+            const int waitTime = 30;
+
+            int currentSpan = 0;
+            
+            Device.StartTimer(new TimeSpan(0, 0, 5), () =>
+            {
+                Timer2Running = false;
+
+                // do something every 10 seconds
+                currentSpan += 5;
+
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    // interact with UI elements
+                    Timer2Progress = $"Remaining {waitTime - currentSpan} seconds";
+
+                });
+
+                if (currentSpan == waitTime)
+                {
+                    Vibration.Vibrate();
+
+                    var notification = new NotificationRequest()
+                    {
+                        BadgeNumber = 1,
+                        Description = "Step 2 ready",
+                        Title = "Step ready",
+                        ReturningData = "This data belongs to step 2",
+                        NotificationId = 10,
+                        Android = new AndroidOptions
+                        {
+                            ChannelId = "ChannelForAlerts",
+                            Ongoing = false,
+                            Priority = NotificationPriority.Max,
+                            VisibilityType = AndroidVisibilityType.Private,
+                        },
+
+                    };
+
+                    NotificationCenter.Current.Show(notification);
+
+//                    App.Current.MainPage.DisplayAlert("Timer finished", "Timer is afgelopen", "OK");
+                    Timer2Running = true;
+                }
+
+                return currentSpan < waitTime; // runs again, or false to stop
+            });
+        }
+
+        
     }
 }
